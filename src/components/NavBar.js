40ad62819b022fc851795b758a84ed31a31dcad8
mad-99 <madhurmaheshwari1999@gmail.com>
2022-07-20 18:03:29 +0530

    20-07-2022

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,24 @@ import { useEffect, useState } from "react";
 import { Navbar,Container } from "react-bootstrap";
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
-    const [scolled, seScrolled] = use(false);
+    const [scolled, seScrolled] = useState(false);
+    useEffect( () => {
+      const onScroll = () =>{
+        if(window.scrollY > 50){
+          seScrolled(true);
+        } else{
+          seScrolled(false);
+        }
+      }
+      window.addEventListener("scroll", onScroll);
 
-
+      return () => window.removeEventListener("scroll",onScroll);
+    }, [])
+    const onUpdateActiveLink = (value) =>{
+      setActiveLink(value);
+    }
   return (
-    <Navbar  expand="lg">
+    <Navbar  expand="lg" className={scrolled ? "scrolled":""}>
       <Container>
         <Navbar.Brand href="#home">
             <img src={''} alt ="Logo"/>
@@ -16,9 +29,9 @@ export const NavBar = () => {
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#skill">Skills</Nav.Link>  
-            <Nav.Link href="#project">Projects</Nav.Link>    
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() =>onUpdateActiveLink('home')} >Home</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() =>onUpdateActiveLink('skills')}>Skills</Nav.Link>  
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link' } onClick={() =>onUpdateActiveLink('projects')}>Projects</Nav.Link>    
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
